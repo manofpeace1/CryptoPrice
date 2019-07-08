5ba2a02eb0e96c729aba2c16fe2eb390e0e8a4d1
manofpeace1 <sysbobby@gmail.com>
2019-07-08 22:16:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import script
+
+
+def main():
+    script.request_crypto_info()
+    script.get_my_crypto_info()
+    script.calc_profitability()
+
+
+if __name__ == '__main__':
+    main()
---
+++ script.py
@@ -0,0 +1,34 @@
+import requests
+from config import my_crypto_wallet
+
+
+def request_crypto_info():
+    global json_response
+    currency = 'EUR'
+    url = 'https://api.alternative.me/v1/ticker/?convert=' + currency
+    send_request = requests.get(url)
+    json_response = send_request.json()
+
+
+def get_my_crypto_info():
+    i = 0
+    while i < len(json_response):
+        for item in my_crypto_wallet:
+            if item == json_response[i]['symbol']:
+                my_crypto_wallet[item]['current_price_eur'] = float(
+                    json_response[i]['price_eur'])
+        i += 1
+
+
+def calc_profitability():
+    overall_profit = 0
+    for item in my_crypto_wallet:
+        current_total_price = my_crypto_wallet[item]['current_price_eur'] * \
+            my_crypto_wallet[item]['owned']
+        original_total_price = my_crypto_wallet[item]['buy_price_eur'] * \
+            my_crypto_wallet[item]['owned']
+        each_currency_profit = current_total_price - original_total_price
+        overall_profit += each_currency_profit
+        print(item, "%.2f" % each_currency_profit, '€')
+
+    print('\nTOTAL', "%.2f" % overall_profit, '€\n')
